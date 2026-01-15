a850c665ce2435b9a8d58864de68321918cea129
HeynanCharles <charlesheynanp@gmail.com>
2026-01-15 01:11:40 -0300

    criação do esqueleto do código

--- classe de heroi.js
+++ classe de heroi.js
@@ -4,4 +4,25 @@ class hero{
         this.age = age
         this.type = type
     }
+    atacar(){
+        let ataque
+
+        switch(this.type){
+            case "":
+                ataque = ""
+                break
+            
+            case "":
+                ataque = ""
+                break
+
+            case "":
+                ataque = ""
+                break
+
+            case "":
+                ataque = ""
+                break
+    }
+    console.log(`O ${this.type} atacou usando ${ataque}`)
 }
